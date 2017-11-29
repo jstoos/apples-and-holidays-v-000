d4b402fa00f4d8947aba54b9254b610221dce2c8
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-29 21:29:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,9 +84,9 @@ def all_holidays_with_bbq(holiday_hash)
         end
       end
     end
-    puts list
-  end
 
+  end
+    puts list
   #return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 

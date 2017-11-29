c00bea913fc1d8c78af52c03297f1b0f6a03dc31
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-29 20:02:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,11 +63,10 @@ def all_supplies_in_holidays(holiday_hash)
     season_data.each do |name_of_holiday, holiday_data|
       list = "  #{name_of_holiday.to_s.split.map(&:capitalize!).join(' ')}: "
         holiday_data.each do |particular_supply|
-      list2 = " #{particular_supply.split.map(&:capitalize!).join(' ')}:" 
+      list2 = " #{particular_supply.split.map(&:capitalize!).join(' ')}:"
         end
       puts "#{list} {list2}""
 
-    end
   end
 end
 

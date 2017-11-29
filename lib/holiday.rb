6fac3f13fd364221d38912bf4a988e847f992f8d
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-29 20:31:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
           supplies << particular_supply.split.map(&:capitalize!).join(' ')
         end
         puts "  #{list}: "
-        supplies.each { |chr| "#{supplies.join(", ")} }
+        supplies.each do |things| "#{things.join(", ")} }
     end
   end
 end
